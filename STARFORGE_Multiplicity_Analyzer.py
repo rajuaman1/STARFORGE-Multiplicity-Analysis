3dd722d37f724b46612b518c1c6839ef412bf184
Aman Raju <rajuaman@gmail.com>
2021-09-23 12:15:24 -0500

    Minor Fix
    CF Observations not defined in Multi_plot

--- STARFORGE_Multiplicity_Analyzer.py
+++ STARFORGE_Multiplicity_Analyzer.py
@@ -5404,6 +5404,8 @@ def Multi_Plot(which_plot,Systems,Files,Filenames,Snapshots = None,log = False,u
             observation_mass_width = [0.0195,0.015,0.0075,0.045,0.0375,0.075,0.15,0.25,0.125,0.125,0.325,0.4,1.5,1.5,4.5,16.5]
             observation_MF = [0.08,0.15,0.19,0.20,0.19,0.23,0.3,np.nan,0.42,0.5,0.47,0.68,0.81,0.89,0.93,0.96]
             observation_MF_err = [0.06,0.04,0.07,0.04,0.03,0.02,0.02,np.nan,0.03,0.04,0.03,0.07,0.06,0.05,0.04,0.04]
+            observation_CF = [0.08,0.16,0.19,0.20,0.21,0.27,0.38,0.60,np.nan,np.nan,0.62,0.99,1.28,1.55,1.8,2.1]
+            observation_CF_err = [0.06,0.04,0.07,0.04,0.03,0.03,0.03,0.04,np.nan,np.nan,0.04,0.13,0.17,0.24,0.3,0.3]
             if multiplicity == 'MF':
                 for i in range(len(observation_mass_center)):
                     if i == 0:
